0c8d59c4941e75977ea6659b7bd1d68a171849d6
mehakgulati20 <megulati@amd.com>
2026-05-27 22:10:16 -0700

    Add files via upload

---
+++ src/feature_engineering/features.py
@@ -0,0 +1,13 @@
+def bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif bmi < 25:
+        return "Normal"
+    elif bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
+
+
+def lifestyle_score(exercise, alcohol, smoking):
+    return exercise - alcohol - smoking
